36a32b9b660a428d7c8f6aeb701537913ce6a2e0
nandan <nandanyeri206@gmail.com>
2026-05-26 17:18:40 +0530

    Enhance Hospital schema with additional fields
    Added new fields for image, rating, location, and description to the Hospital schema.

--- models/Hospital.ts
+++ models/Hospital.ts
@@ -1,15 +1,14 @@
 import mongoose, { Schema, models, model } from "mongoose";
 
-const HospitalSchema = new Schema(
-  {
-    name: String,
-    city: String,
-    specialization: String,
-  },
-  {
-    timestamps: true,
-  }
-);
+const HospitalSchema = new Schema({
+  name: String,
+  city: String,
+  specialization: String,
+  image: String,
+  rating: Number,
+  location: String,
+  description: String,
+});
 
 const Hospital =
   models.Hospital || model("Hospital", HospitalSchema);
